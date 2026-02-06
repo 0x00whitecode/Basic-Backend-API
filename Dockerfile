889fb184d0c61d164e157695d39a532181dbbe3e
0x00whitecode <x10tion007@gmail.com>
2026-02-06 03:42:01 +0100

    chore: push workspace to Basic-Backend-API

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
-# -------- Build stage --------
-FROM rust:1.75 as builder
+FROM rust:1.59.0
 
 WORKDIR /app
-COPY Cargo.toml Cargo.lock ./
-RUN mkdir src && echo "fn main() {}" > src/main.rs
-RUN cargo build --release
+
+RUN apt update && apt install lld clang -y 
 
 COPY . .
+
 RUN cargo build --release
 
-# -------- Runtime stage --------
-FROM debian:bookworm-slim
-
-RUN apt-get update && apt-get install -y libpq5 && rm -rf /var/lib/apt/lists/*
-
-WORKDIR /app
-COPY --from=builder /app/target/release/Cat_API .
-
-EXPOSE 8080
-CMD ["./Cat_API"]
+ENTRYPOINT ["./target/release/hng_task_one"]
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
-use actix_files::{NamedFile, Files};
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{web, App, HttpServer, Responder, HttpRequest};
+use serde::Serialize;
 
 
-async fn index() -> Result<NamedFile> {
-    Ok(NamedFile::open("./static/index.html")?)
+#[derive(Serialize)]
+pub struct RespondeJson {
+    pub success:String,
+    pub message:String,
+}
+#[derive(Serialize)]
+pub struct Identity {
+    pub name:String,
 }
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+async fn health_check () -> impl Responder {
+    let res = vec![
+        RespondeJson {
+            success: "true".to_string(),
+            message: "API is running".to_string()
+        }
+    ];
+    return web::Json(res);
 }
+
+async fn welcome_message () -> impl Responder{
+    let res = vec![
+        RespondeJson {
+            success: "true".to_string(),
+            message: "welcome to HNG Task 0x0001".to_string()
+        } 
+    ];
+    return web::Json(res);
+}
+
+async fn welcome_message_with_name (req: HttpRequest) -> impl Responder{
+   let  name = req.match_info().get("name").unwrap_or("world");
+   let res = vec![
+    Identity{
+        name: name.to_string() + " we have your detail now",
+    }
+   ]; 
+   
+   return web::Json(res);
+   
+
+}
+
+
+
+// build reusable configuration
+fn api_config(cfg: &mut web::ServiceConfig){
+    cfg.service(
+        web::scope("/api")
+        .route("/", web::get().to(welcome_message))
+        .route("/{name}", web::get().to(welcome_message_with_name))
+        .route("/health", web::get().to(health_check))
+        //welcome_message_with_name
+
+    );
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Listen on the port 8080");
+    println!("Listening on port 8080");
     HttpServer::new(|| {
         App::new()
-        .service(
-            Files::new("/static", "static")
-            .show_files_listing(),
-        )
-        .service(
-            Files::new("/image", "image")
-            .show_files_listing(),
-        )
-            .route("/hell", web::get().to(hello))
-            .route("/", web::get().to(index))
+       .configure(api_config)
+       
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
+
+
+
